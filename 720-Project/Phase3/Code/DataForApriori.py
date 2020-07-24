5b01881e3f55db60126601f04c3ebc615f1a736e
nari97 <narayananak12@gmail.com>
2020-07-24 15:04:50 -0400

    Documented Code

--- 720-Project/Phase3/Code/DataForApriori.py
+++ 720-Project/Phase3/Code/DataForApriori.py
@@ -3,6 +3,8 @@ import pickle
 
 data = pd.read_csv(r'C:\Users\nari9\OneDrive\Documents\PG\Assignments\Summer3\BDA\720-Project\720-Project\Data\MergedDataMoreSources.csv')
 
+
+#Filter out data from year 2008 to 2009
 data = (data[data['Date']>='2008'])
 
 data = data[data['Date']<'2009']
@@ -12,6 +14,8 @@ all_dates = data['Date'].unique()
 
 adj_close_per_day = []
 i = 0
+
+#Get all Adj. Close prices into a list
 for date in all_dates:
     #print (data[data['Date'] == date]['Adj. Close'])
     adj_close_per_day.append(data[data['Date'] == date]['Adj. Close'].tolist())
@@ -19,6 +23,8 @@ for date in all_dates:
 
 apriori_dataset = []
 companies = data['Company'].unique()
+
+#Check for each day and company if the current price is higher than the previous price
 for i in range(1, len(adj_close_per_day)):
     l = []
     for j in range(0, 41):
@@ -29,9 +35,10 @@ for i in range(1, len(adj_close_per_day)):
 
 print (len(adj_close_per_day))
 
+#Dump the dataset to a file
+f = open('stockTransactions.dump', 'wb')
+pickle.dump(apriori_dataset, f)
 
-#f = open('stockTransactions.dump', 'wb')
-#pickle.dump(apriori_dataset, f)
 #apriori_dataframe = pd.DataFrame(apriori_dataset, columns=companies)
 
 #apriori_dataframe.to_csv(r'C:\Users\nari9\OneDrive\Documents\PG\Assignments\Summer3\BDA\720-Project\720-Project\Data\Apriori2008Companies.csv')
